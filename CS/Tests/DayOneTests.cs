867f4bf12c917f4aadfc40183e37ab79dc4ddef9
spectre <nathancraigforsyth@gmail.com>
2023-12-02 23:28:44 +1300

    Complete Day 1, Part 2.
    DayOneTests:
    - refactor to separate parts by class.
    
    Utilities:
    - add new Enums; NumberStrings.
    - add ExtractNumbersAndCalculateTotalCalibrationValue method to StringHelpers.
    - extract common code to new method, CalculateCalibrationLine.
    
    DayOne.Program:
    - separate parts, implement part 2 and add time validation.
    
    Shared:
    - add DayOne test input for part 2.

--- CS/Tests/DayOneTests.cs
+++ CS/Tests/DayOneTests.cs
@@ -1,39 +1,84 @@
 using Utilities.Helpers;
+using Utilities.Enums;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Tests
 {
-    [TestClass]
     public class DayOneTests
     {
-        [TestMethod]
-        public void File_WithValidPath_IsNotNull()
+        [TestClass]
+        public class DayOneTests_PartOne
         {
-            // Arrange
-            List<string> fileContent = FileHelpers.GetFileContentFromRelativePath("\\Shared\\Input\\DayOne\\TestInput.txt");
+            [TestMethod]
+            public void File_WithValidPath_IsNotNull()
+            {
+                // Arrange
+                List<string> fileContent = FileHelpers.GetFileContentFromRelativePath("\\Shared\\Input\\DayOne\\TestInput.txt");
 
-            // Assert
-            Assert.IsNotNull(fileContent);
+                // Assert
+                Assert.IsNotNull(fileContent);
+            }
+
+            [TestMethod]
+            public void File_WithValidPath_GetsContent()
+            {
+                // Arrange
+                List<string> fileContent = FileHelpers.GetFileContentFromRelativePath("\\Shared\\Input\\DayOne\\TestInput.txt");
+
+                // Assert
+                Assert.IsTrue(fileContent.Count > 0);
+            }
+
+            [TestMethod]
+            public void File_WithValidPath_ValidatesTotalCalibrationValue()
+            {
+                // Arrange
+                List<string> fileContent = FileHelpers.GetFileContentFromRelativePath("\\Shared\\Input\\DayOne\\TestInput.txt");
+
+                // Act
+                int totalCalibrationValue = NumberHelpers.CalculateTotalCalibrationValue(fileContent);
+
+                // Assert
+                Assert.AreEqual(142, totalCalibrationValue);
+            }
         }
 
-        [TestMethod]
-        public void File_WithValidPath_GetsContent()
+        [TestClass]
+        public class DayTwoTests_PartTwo
         {
-            // Arrange
-            List<string> fileContent = FileHelpers.GetFileContentFromRelativePath("\\Shared\\Input\\DayOne\\TestInput.txt");
+            [TestMethod]
+            public void File_WithValidPath_IsNotNull()
+            {
+                // Arrange
+                List<string> fileContent = FileHelpers.GetFileContentFromRelativePath("\\Shared\\Input\\DayOne\\TestInput_2.txt");
 
-            // Assert
-            Assert.IsTrue(fileContent.Count > 0);
-        }
+                // Assert
+                Assert.IsNotNull(fileContent);
+            }
 
-        [TestMethod]
-        public void File_WithValidPath_ValidatesTotalCalibrationValue()
-        {
-            // Arrange
-            List<string> fileContent = FileHelpers.GetFileContentFromRelativePath("\\Shared\\Input\\DayOne\\TestInput.txt");
-            var totalCalibrationValue = NumberHelpers.CalculateTotalCalibrationValue(fileContent);
+            [TestMethod]
+            public void File_WithValidPath_GetsContent()
+            {
+                // Arrange
+                List<string> fileContent = FileHelpers.GetFileContentFromRelativePath("\\Shared\\Input\\DayOne\\TestInput_2.txt");
 
-            // Assert
-            Assert.AreEqual(142, totalCalibrationValue);
+                // Assert
+                Assert.IsTrue(fileContent.Count > 0);
+            }
+
+            [TestMethod]
+            public void Strings_WithValidData_ExtractsValidIntegerStringsFromInputString()
+            {
+                // Arrange
+                List<string> fileContent = FileHelpers.GetFileContentFromRelativePath("\\Shared\\Input\\DayOne\\TestInput_2.txt");
+
+                // Act
+                int totalCalibrationValue = StringHelpers.ExtractNumbersAndCalculateTotalCalibrationValue(fileContent);
+
+                // Assert
+                Assert.AreEqual(281, totalCalibrationValue);
+            }
         }
     }
 }
